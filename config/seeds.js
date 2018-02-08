0181b8c9fe042c670dbb033306b61add86a6cb84
j <j@debjan.ciudadanoi>
2018-02-08 16:03:31 -0300

    added db seed

---
+++ config/seeds.js
@@ -0,0 +1,23 @@
+/**
+ * Sails Seed Settings
+ * (sails.config.seeds)
+ *
+ * Configuration for the data seeding in Sails.
+ *
+ * For more information on configuration, check out:
+ * http://github.com/frostme/sails-seed
+ */
+module.exports.seeds = {
+  disable: false,
+  user: {
+    data: [
+      {
+        name: 'j',
+        email:  'j',
+        password: 'j',
+        passwordConfirmation: 'j',
+      }
+    ],
+    overwrite: false
+  }
+}
