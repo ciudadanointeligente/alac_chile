786a7271f721db81403ae0021c631bf42f316902
root <root@localhost.localdomain>
2018-02-12 14:44:17 -0300

    caso de cookies inicial (reparado)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,48 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    console.log(document.cookie);
+
     var arrLang= {
       'en': {
-        'Hola': 'Good day'
+        'Hola': 'Good day',
+        'alac': 'What is alac?',
+        'como': 'How does it work?',
+        'buzon': 'Denunciation mailbox',
+        'articulos': 'Educational articles',
+        'preguntas': 'Frequent questions'
       },
       'es': {
-        'Hola': 'Buen día'
+        'Hola': 'Buen día',
+        'alac': '¿Qué es alac?',
+        'como': '¿Cómo funciona?',
+        'buzon': 'Buzón de denuncia',
+        'articulos': 'Artículos educativos',
+        'preguntas': 'Preguntas frecuentes'
       },
       'ht': {
-        'Hola': 'Bonjour'
+        'Hola': 'Bonjour',
+        'alac': "Qu'est ce que c'est alac?",
+        'como': 'Comment ça marche?',
+        'buzon': 'Denonciation boîte aux lettres',
+        'articulos': 'Articles éducatifs',
+        'preguntas': 'Questions fréquentes'
       },
       'pt': {
-        'Hola': 'OI!'
+        'Hola': 'OI!',
+        'alac': 'O que é alac?',
+        'como': 'Como funciona?',
+        'buzon': 'Caixa de correio de denúncia',
+        'articulos': 'Artigos educativos',
+        'preguntas': 'Perguntas frequentes'
       }
     };
     function getCookie(name) {
       var value = "; " + document.cookie;
       var parts = value.split("; " + name + "=");
       if (parts.length == 2) return parts.pop().split(";").shift();
+    };
+
+    if (getCookie("lang") == null) {
+        document.cookie = "lang=es";
     };
 
     $('.lang').each(function(index,element){
