dd43fb49017cb0a777401eb5bd3d5184cac63e4d
andresGooz <36235324+andresGooz@users.noreply.github.com>
2018-02-22 11:15:58 -0300

    Update sockets.js

--- config/sockets.js
+++ config/sockets.js
@@ -11,7 +11,7 @@
  */
 
 module.exports.sockets = {
-
+  onlyAllowOrigins: ["http://www.denunciacorrupcion.cl", "http://denunciacorrupcion.cl"]
 
   /***************************************************************************
   *                                                                          *
